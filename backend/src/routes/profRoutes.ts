b7935c7b1688cd2834504b35d07c93210ef01385
Anthony Kumar <me@anthonykumar.com>
2022-11-21 20:01:13 -0500

    added requireAuth code commented

--- backend/src/routes/profRoutes.ts
+++ backend/src/routes/profRoutes.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 import { getAllProfs, addProfs, registerProfFromFile} from '../controllers/profController';
 import multer from "multer";
+import requireAuth from '../middleware/requireAuth';
 
 const upload = multer();
 const router = express.Router();
+
+// require authentication for all course routes
+// router.use(requireAuth);
 
 router.route("/").get(getAllProfs);
 router.route("/add").post(addProfs);
