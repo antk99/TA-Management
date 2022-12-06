e252e51a44a6cf7c9a7f326e6662ca52175e1928
Anthony Kumar <me@anthonykumar.com>
2022-12-05 20:18:13 -0500

    added all application routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,8 +4,17 @@ import connectDB from "./config/db.config";
 import userRoutes from './routes/userRoutes';
 import profRoutes from './routes/profRoutes';
 import courseRoutes from './routes/courseRoutes';
+import taRoutes from './routes/taRoutes';
+import wishlistRoutes from './routes/wishlistRoutes';
+import ratingRoutes from './routes/ratingRoutes';
+import studentRoutes from './routes/studentRoutes';
+import performanceLogRoutes from './routes/performanceLogRoutes';
+import taCohortRoutes from './routes/taCohortRoutes';
+import requireAuth from './middleware/requireAuth';
 
 const app = express();
+
+// TODO: put in env file
 const port = 3000;
 
 // Basic express setup
@@ -19,11 +28,23 @@ app.use((req: Request, res: Response, next: any) => {
     next();
 });
 
+// Authentication middleware
+app.use(requireAuth);
+
+// Application routes
+// TODO: for all route controllers, only provide info for the current user passed in request.body.user
+// e.g. if user._id = 1, only allow user 1 to access their own info & not other users' info even if authenticated
 app.use("/api/users", userRoutes);
 app.use("/api/prof", profRoutes);
 app.use("/api/course", courseRoutes);
+app.use("/api/student", studentRoutes);
+app.use("/api/ta", taRoutes);
+app.use("/api/review", ratingRoutes);
+app.use("/api/performanceLog", performanceLogRoutes);
+app.use("/api/wishlist", wishlistRoutes);
+app.use("/api/cohort", taCohortRoutes);
 
-
+// Start the server
 app.listen(port, () => {
     console.log('Backend is running on port: ' + port)
 })
