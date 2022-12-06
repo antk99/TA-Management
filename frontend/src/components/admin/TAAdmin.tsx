42fa389f0de80c8c53295d7c81a1851502ced4a9
Anthony Kumar <me@anthonykumar.com>
2022-12-05 23:03:12 -0500

    table loads data from db

--- frontend/src/components/admin/TAAdmin.tsx
+++ frontend/src/components/admin/TAAdmin.tsx
@@ -1,57 +1,53 @@
-import React from "react";
+import React, { useContext } from "react";
+import { UserContext } from "../../App";
 import { TA } from "../../classes/TA";
+import { useHttp } from "../../hooks/useHttp";
 import TACourseHistory from "./TACourseHistoryTable";
 import TAInfo from "./TAInfo";
-
-const dummyTA = {
-    name: "John Doe",
-    email: "johndoe@email.com",
-    studentID: "123456789",
-    legalName: "John Bob Doe",
-    level: "Undergraduate",
-    supervisorName: "John Doe",
-    isPriority: true,
-    hours: 20,
-    dateApplied: "2021-01-01",
-    location: "Online",
-    phone: "1234567890",
-    degree: "BSc",
-    coursesAppliedFor: ["CSC108", "CSC148"],
-    openToOtherCourses: true,
-    notes: "This a note for TA John Doe",
-    currCourses: ["CSC108", "CSC148", "CSC209"],
-    prevCourses: ["CSC207", "CSC236", "CSC258"]
-};
-
-const dummyTA2 = { ...dummyTA, name: "Jane Doe", email: "JaneDoe@mail.mcgill.ca", studentID: "987654321", currCourses: ["COMP 302", "COMP 307", "COMP 303"] };
 
 const TAAdmin = () => {
     // stores the studentID of the TA that is currently being viewed (TAInfo)
     // if no TA is being viewed, this is set to null --> display list of all TAs (TACourseHistory)
     const [studentID, setStudentID] = React.useState<string>("");
+    const { user } = useContext(UserContext);
+    const [TAs, setTAs] = React.useState<Array<TA>>([]);
 
-    const [TAs, setTAs] = React.useState<Array<TA>>([
-        dummyTA, dummyTA2, dummyTA, dummyTA, dummyTA, dummyTA, dummyTA, dummyTA, dummyTA, dummyTA, dummyTA, dummyTA, dummyTA, dummyTA, dummyTA, dummyTA2, dummyTA, dummyTA, dummyTA, dummyTA, dummyTA, dummyTA, dummyTA, dummyTA, dummyTA, dummyTA, dummyTA, dummyTA, dummyTA, dummyTA2, dummyTA, dummyTA, dummyTA, dummyTA, dummyTA, dummyTA, dummyTA, dummyTA, dummyTA, dummyTA, dummyTA, dummyTA,
-    ]);
-
-    const fetchAllTAs = async () => {
-        try {
-            // const res = await fetch("http://127.0.0.1:3000/api/tas");
-            // const json = await res.json();
-            // setTAs(json.TAs);
-        } catch (err) {
-            console.log(err);
-        }
-    };
-
-    const modifyTACurrCourses = (studentID, newCurrCourses) => {
-        setTAs(TAs.map((ta) => (studentID === ta.studentID ? { ...ta, currCourses: newCurrCourses } : ta)));
-    };
+    const { isLoading, error, sendRequest: fetchAllTAs } = useHttp(
+        { url: "http://localhost:3000/api/ta" },
+        async (responseData) => {
+            const taObject = [];
+            for (const d of responseData.TAs) {
+                const userResponse = await fetch(
+                    "http://localhost:3000/api/users/" + d.ta,
+                    { headers: { Authorization: "Bearer " + user.token } }
+                );
+                let item = {
+                    currCourses: d.currCourses,
+                    prevCourses: d.prevCourses,
+                    studentID: d.studentID,
+                }
+                if (userResponse) {
+                    const userResponseData = await userResponse.json();
+                    const firstName = userResponseData.user.firstName;
+                    const lastName = userResponseData.user.lastName;
+                    item["name"] = firstName + " " + lastName;
+                    item["email"] = userResponseData.user.email;
+                }
+                taObject.push(item);
+            }
+            setTAs(taObject);
+        },
+        user.token
+    );
 
     React.useEffect(() => {
         // Load data
         fetchAllTAs();
     }, []);
+
+    const modifyTACurrCourses = (studentID, newCurrCourses) => {
+        setTAs(TAs.map((ta) => (studentID === ta.studentID ? { ...ta, currCourses: newCurrCourses } : ta)));
+    };
 
     return (
         <>
