21149b8cd6c9cb3144afd23854dd41ca33d00c17
Anthony Kumar <me@anthonykumar.com>
2022-12-12 19:22:36 -0500

    removed register ta from file route

--- backend/src/routes/taRoutes.ts
+++ backend/src/routes/taRoutes.ts
@@ -1,14 +1,11 @@
 import express from 'express';
-import multer from "multer";
-import { getAllTAs, addTA, registerTAFromFile, addTACurrCourse, removeTACurrCourse } from '../controllers/taController';
+import { getAllTAs, addTA, addTACurrCourse, removeTACurrCourse } from '../controllers/taController';
 
-const upload = multer();
 const router = express.Router();
 
 router.route("/").get(getAllTAs);
 router.route("/add").post(addTA);
 router.route("/addCurrCourse").post(addTACurrCourse);
 router.route("/removeCurrCourse").delete(removeTACurrCourse);
-router.route("/upload").post(upload.single("csvFile"), registerTAFromFile);
 
 export default router;
