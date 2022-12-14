2e5c44c96d243b466032c32045265de21508614a
Anthony Kumar <me@anthonykumar.com>
2022-12-14 00:25:27 -0500

    sorted courses

--- frontend/src/classes/TA.ts
+++ frontend/src/classes/TA.ts
@@ -11,5 +11,5 @@ export interface TA {
 
 // converts an array of course registration objects to a string of course numbers
 export const courseRegArrayToString = (arr: Array<any>, delim: string = ", ") => {
-    return arr.map((courseReg) => courseReg.courseNumber).join(delim);
+    return arr.map((courseReg) => courseReg.courseNumber).sort((a, b) => a > b ? 1 : -1).join(delim);
 };
