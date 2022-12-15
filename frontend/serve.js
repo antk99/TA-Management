62c5687ea7845a17e35e84a2fb26e8845e3e01a8
Anthony Kumar <me@anthonykumar.com>
2022-12-15 15:37:23 -0500

    created production serve.js

---
+++ frontend/serve.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+/**
+ * Serve index.html from the production Build folder for all requests
+ */
+app.get("/*", (req, res) => {
+    console.log(`${req.method} ${req.url}`);
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+/**
+ * Start the server at port 3000
+ */
+app.listen(3000, () => {
+    console.log("Frontend server is running on port 3000");
+});
