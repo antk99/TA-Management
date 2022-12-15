7b785995ac88dafc1940e7a3e3e9a6c26328c094
Anthony Kumar <me@anthonykumar.com>
2022-12-14 20:20:45 -0500

    extracted mongo string to .env file

--- backend/src/config/db.config.ts
+++ backend/src/config/db.config.ts
@@ -1,15 +1,14 @@
-// require('dotenv').config()
-// this needs to be managed using a .env file
-// Put the Mongo connection string in the file as MONGO_CONNECTION_STRING="xxxx"
-// const url = process.env.MONGO_CONNECTION_STRING;
-
-
+require('dotenv').config()
 import mongoose from 'mongoose';
 import { ConnectionOptions } from 'tls';
 
+if (!process.env.MONGO_CONNECTION_STRING)
+    console.log("MONGO_CONNECTION_STRING not set in env file, using default connection string");
+const MongoConnectionString = process.env.MONGO_CONNECTION_STRING as string || "mongodb://localhost:27017/comp307";
+
 const connectDB = async () => {
     try {
-        const connect = await mongoose.connect("mongodb://localhost:27017/comp307" as string, {
+        const connect = await mongoose.connect(MongoConnectionString, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             family: 4,
