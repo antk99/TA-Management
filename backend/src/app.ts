7c385cfe8a3312d076290bc739654506f27f60a5
Anthony Kumar <me@anthonykumar.com>
2022-12-15 19:11:40 -0500

    added production code for routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -12,6 +12,7 @@ import performanceLogRoutes from './routes/performanceLogRoutes';
 import taCohortRoutes from './routes/taCohortRoutes';
 import requireAuth from './middleware/requireAuth';
 import courseQuotaRoutes from './routes/courseQuotaRoutes';
+import standardizeURL from './middleware/standardizeURL';
 require('dotenv').config();
 
 const app = express();
@@ -21,10 +22,16 @@ if (!process.env.PORT)
 
 const port = process.env.PORT || 3000;
 
+// In production, the /api prefix is removed from client requests, so the server does not need to add it
+// in development, the /api prefix is not removed from client requests, so the server needs to add it
+const urlPrefix = process.env.PRODUCTION ? "" : "/api";
+
 // Basic express setup
 app.use(cors());
 app.use(express.json());
 connectDB();
+
+app.use(standardizeURL);
 
 // Logging middleware
 app.use((req: Request, res: Response, next: any) => {
@@ -32,26 +39,26 @@ app.use((req: Request, res: Response, next: any) => {
     next();
 });
 
-app.use("/api/users", userRoutes);
+// Some routes do not require authentication
+app.use(urlPrefix + "/users", userRoutes);
 
-// Authentication middleware
 app.use(requireAuth);
 
-// Application routes
+// Application routes that require authentication
 // TODO: for all route controllers, only provide info for the current user passed in request.body.user
 // e.g. if user._id = 1, only allow user 1 to access their own info & not other users' info even if authenticated
-app.use("/api/prof", profRoutes);
-app.use("/api/course", courseRoutes);
-app.use("/api/student", studentRoutes);
-app.use("/api/ta", taRoutes);
-app.use("/api/rating", ratingRoutes);
-app.use("/api/performanceLog", performanceLogRoutes);
-app.use("/api/wishlist", wishlistRoutes);
-app.use("/api/cohort", taCohortRoutes);
-app.use("/api/courseQuota", courseQuotaRoutes);
+app.use(urlPrefix + "/prof", profRoutes);
+app.use(urlPrefix + "/course", courseRoutes);
+app.use(urlPrefix + "/student", studentRoutes);
+app.use(urlPrefix + "/ta", taRoutes);
+app.use(urlPrefix + "/rating", ratingRoutes);
+app.use(urlPrefix + "/performanceLog", performanceLogRoutes);
+app.use(urlPrefix + "/wishlist", wishlistRoutes);
+app.use(urlPrefix + "/cohort", taCohortRoutes);
+app.use(urlPrefix + "/courseQuota", courseQuotaRoutes);
 
 // Start the server
 app.listen(port, () => {
-    console.log('Backend is running on port: ' + port)
+    console.log(`${process.env.PRODUCTION ? "Production" : "Development"} backend server running on port ${port}`);
 })
 
